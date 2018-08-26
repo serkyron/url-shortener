3f666194e9d7248bd39061b94641afc7482a9426
Sergei Krivosheenko <serkyron@gmail.com>
2018-08-26 17:35:00 +0300

    Changed routes configuration to use annotation routes.

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -3,11 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class BaseController extends AbstractController
 {
+    /**
+     * Corresponds to application home page
+     *
+     * @Route("/", name="home")
+     */
     public function index()
     {
         return $this->render('home.html.twig');
+    }
+
+    /**
+     * API method used to create a short URL
+     *
+     * @Route("/api/shorten/url", name="shorten")
+     * @Method({"POST"})
+     */
+    public function shorten()
+    {
+        
     }
 }
